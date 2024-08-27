895ae422a340c76c0f6cf8c680088ba6394407e8
Igor Sulim <igosulim@gmail.com>
2024-08-27 18:50:43 +0200

    Download tokenizer locally

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -8,7 +8,6 @@ from tqdm import tqdm
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
 MODEL_NAME = "facebook/m2m100_418M"
-FILENAME = "m2m100_418M"
 
 warnings.filterwarnings("ignore")
 
@@ -17,12 +16,14 @@ if __name__ == "__main__":
     local_dir = Path("models")
     local_dir.mkdir(exist_ok=True)
 
-    local_model = Path(local_dir, FILENAME)
+    local_model = Path(local_dir, MODEL_NAME, "model")
+    local_tokenizer = Path(local_dir, MODEL_NAME, "tokenizer")
 
     print(f"Downloading model: {MODEL_NAME}")
     with tqdm() as pbar:
-        model = M2M100ForConditionalGeneration.from_pretrained(MODEL_NAME)
         tokenizer = M2M100Tokenizer.from_pretrained(MODEL_NAME)
-    model.save_pretrained(local_model)
-
-    print(f"Model saved to: {local_model}")
+        tokenizer.save_pretrained(local_tokenizer)
+        print(f"Tokenizer saved to: {local_tokenizer}")
+        model = M2M100ForConditionalGeneration.from_pretrained(MODEL_NAME)
+        model.save_pretrained(local_model)
+        print(f"Model saved to: {local_model}")
