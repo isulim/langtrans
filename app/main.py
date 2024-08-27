62c08b4db59bb393078ef3f63c32e24b8eac3d52
Igor Sulim <igosulim@gmail.com>
2024-08-27 17:54:44 +0200

    classes implementing models

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from litestar import Litestar, get
+
+
+
+
+
+app = Litestar()
---
+++ app/models.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+from py3langid.langid import LanguageIdentifier, MODEL_FILE
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+
+from app.config import AVAILABLE_LANGS
+
+
+class LangIdentifier:
+    """Implementation of LangID language identifier."""
+
+    def __init__(self):
+        self.identifier = LanguageIdentifier.from_pickled_model(MODEL_FILE, norm_probs=True)
+        self.identifier.set_languages(AVAILABLE_LANGS)
+
+    def identify(self, text: str) -> str:
+        """
+        Identifies the language of the input text.
+
+        Args:
+            text: Text to identify the language.
+
+        Returns:
+            Language code of the identified language.
+        """
+
+        lang = self.identifier.classify(text)[0]
+        return lang
+
+
+class M2MTranslator:
+    """Implementation of M2M100 model with tokenizer
+    (https://huggingface.co/facebook/m2m100_418M)[https://huggingface.co/facebook/m2m100_418M]"""
+
+    def __init__(self):
+        model_path = Path("models/m2m100_418M")
+        if not model_path.exists():
+            raise FileNotFoundError(f"Model in path: {model_path} does not exist.")
+
+        self.model = M2M100ForConditionalGeneration.from_pretrained(model_path)
+        self.tokenizer = M2M100Tokenizer.from_pretrained(model_path)
+
+    def translate(self, text: str, source_lang: str, target_lang: str) -> str:
+        """
+        Translates the input text to the target language.
+
+        Args:
+            text: Text to translate.
+            source_lang: Source language of the text.
+            target_lang: Target language to translate the text.
+
+        Returns:
+            Translated text.
+        """
+
+        self.tokenizer.src_lang = source_lang
+
+        inputs = self.tokenizer(text, return_tensors="pt").input_ids
+        outputs = self.model.generate(inputs, forced_bos_token_id=self.tokenizer.get_lang_id(target_lang))
+        translated_text = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+        return translated_text
