652b7c6ea93195ef91fb68fe068540699e54f4b5
Igor Sulim <igosulim@gmail.com>
2024-08-27 19:15:13 +0200

    Enum for available langs

--- app/config.py
+++ app/config.py
@@ -1,1 +1,17 @@
-AVAILABLE_LANGS = ["de", "en", "el", "es", "fr", "it", "pl", "pt", "ro", "nl"]
+from enum import Enum
+
+MODEL_NAME = "facebook/m2m100_418M"
+AVAILABLE_LANGS = ("de", "en", "el", "es", "fr", "it", "pl", "pt", "ro", "nl")
+
+
+class AvailableLangEnum(str, Enum):
+    ENGLISH = "en"
+    GERMAN = "de"
+    GREEK = "el"
+    SPANISH = "es"
+    FRENCH = "fr"
+    ITALIAN = "it"
+    POLISH = "pl"
+    PORTUGUESE = "pt"
+    ROMANIAN = "ro"
+    DUTCH = "nl"
