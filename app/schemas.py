d8f5c93873600e16f3cdcba53a2f28edc170be81
Igor Sulim <igosulim@gmail.com>
2024-08-27 19:15:50 +0200

    Schemas for requests/responses

--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from app.config import AVAILABLE_LANGS
+from app.config import AVAILABLE_LANGS, AvailableLangEnum
 
 
 class LangDetectionRequest(BaseModel):
-    input_text: str
+    input_text: str = Field(title="Input text", description="Text to identify the language.")
 
 
 class LangDetectionResponse(BaseModel):
-    detected_lang: str
+    detected_lang: str = Field(title="Detected language", description="ISO language code of the detected language.")
 
 
-class TextTranslationRequest(BaseModel):
-    input_text: str
-    source_lang: str
-    target_lang: str
+class TranslationRequest(BaseModel):
+    input_text: str = Field(title="Input text", description="Text to translate.")
+    source_lang: AvailableLangEnum = Field(title="Source language", description="ISO language code of the input text.")
+    target_lang: AvailableLangEnum = Field(title="Target language", description="ISO language code of the output text.")
 
     def __init__(self, **data):
         super().__init__(**data)
@@ -22,8 +22,8 @@ class TextTranslationRequest(BaseModel):
             raise ValueError(f"Target language {self.target_lang} is not supported.")
 
 
-class TextResponse(BaseModel):
-    input_text: str
-    output_text: str
-    source_lang: str
-    target_lang: str
+class TranslationResponse(BaseModel):
+    input_text: str = Field(title="Input text", description="Text to translate.")
+    output_text: str = Field(title="Output text", description="Translated text.")
+    source_lang: AvailableLangEnum = Field(title="Source language", description="ISO language code of the input text.")
+    target_lang: AvailableLangEnum = Field(title="Target language", description="ISO language code of the output text.")
